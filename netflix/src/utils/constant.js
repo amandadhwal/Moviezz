1acf1e48507570b4ba212c052ff76fbbf245c258
Aman Dadhwal <amandadhwal911@gmail.com>
2025-09-15 15:30:21 +0530

    Updated Prod API endpoint in build

--- netflix/src/utils/constant.js
+++ netflix/src/utils/constant.js
@@ -1,4 +1,5 @@
-export const API_END_POINT = "http://jobportal.centralindia.cloudapp.azure.com/api/v1/user";
+export const API_END_POINT = "http://moviezz.centralindia.cloudapp.azure.com/api/v1/user";
+//export const API_END_POINT = "http://localhost:8080/api/v1/user";
 
 export const options = {
     method: 'GET',
